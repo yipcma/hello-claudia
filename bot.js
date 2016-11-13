0e842a105483239e64e22e995ce401a69ac373f4
Andrew Yip <yipcma@gmail.com>
2016-11-13 18:34:48 +0300

    to fix async call to wit

--- bot.js
+++ bot.js
@@ -1,7 +1,41 @@
-var
-  botBuilder = require('claudia-bot-builder'),
-  excuse = require('huh');
+'use strict'
 
-  module.exports = botBuilder(function (request) {
-    return 'Thanks for sending ' + request.text + '. Your message is very important to us, but ' + excuse.get();
-  });
+const botBuilder = require('claudia-bot-builder');
+const rp = require('minimal-request-promise')
+
+function callWit(message, witaiApiKey) {
+
+    const headers = {
+        'Authorization': 'Bearer ' + witaiApiKey
+    };
+
+    const options = {
+        headers: headers
+    };
+
+    rp.get('https://api.wit.ai/message?v=20161113&q=' + message, options).then(function(response) {
+        console.log('got response', response.body, response.headers);
+        return JSON.stringify(response.body);
+    }, function(response) {
+        console.log('got error', response.body, response.headers, response.statusCode, response.statusMessage);
+        return JSON.stringify(response.body);
+    })
+}
+
+const app = botBuilder(function(request, originalApiRequest) {
+    console.log(request);
+
+    return rp.get(`https://graph.facebook.com/v2.6/${request.sender}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${originalApiRequest.env.facebookAccessToken}`).then(response => {
+        const user = JSON.parse(response.body)
+        return {
+            first_name: user.first_name,
+            responseBody: callWit(request.text, originalApiRequest.env.witaiApiKey)
+        }
+    }).then((response) => {
+        return [`Hello ${response.first_name}.`, 'I understood the following:', response.responseBody]
+    })
+});
+
+app.addPostDeployConfig('witaiApiKey', 'WIT.AI API Key:', 'configure-app');
+
+module.exports = app;
