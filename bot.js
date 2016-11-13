56968b403db011903b2acd0d1a756cbe59d2e5f4
Andrew Yip <yipcma@gmail.com>
2016-11-13 20:33:28 +0300

    minimal location extraction for kbus from wit

--- bot.js
+++ bot.js
@@ -3,37 +3,26 @@
 const botBuilder = require('claudia-bot-builder');
 const rp = require('minimal-request-promise')
 
-function callWit(message, witaiApiKey) {
+const app = botBuilder(function(request, originalApiRequest) {
+    console.log(request);
 
     const headers = {
-        'Authorization': 'Bearer ' + witaiApiKey
+        'Authorization': 'Bearer ' + originalApiRequest.env.witaiApiKey
     };
 
     const options = {
         headers: headers
     };
 
-    rp.get('https://api.wit.ai/message?v=20161113&q=' + message, options).then(function(response) {
-        console.log('got response', response.body, response.headers);
-        return JSON.stringify(response.body);
+    return rp.get('https://api.wit.ai/message?v=20161113&q=' + request.text, options).then(function(response) {
+        const reply = JSON.parse(response.body)
+        console.log('got response', reply);
+        return 'Your request was ' + reply._text;
     }, function(response) {
-        console.log('got error', response.body, response.headers, response.statusCode, response.statusMessage);
-        return JSON.stringify(response.body);
+        console.log('got error', response.body);
+        return 'error!';
     })
-}
 
-const app = botBuilder(function(request, originalApiRequest) {
-    console.log(request);
-
-    return rp.get(`https://graph.facebook.com/v2.6/${request.sender}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${originalApiRequest.env.facebookAccessToken}`).then(response => {
-        const user = JSON.parse(response.body)
-        return {
-            first_name: user.first_name,
-            responseBody: callWit(request.text, originalApiRequest.env.witaiApiKey)
-        }
-    }).then((response) => {
-        return [`Hello ${response.first_name}.`, 'I understood the following:', response.responseBody]
-    })
 });
 
 app.addPostDeployConfig('witaiApiKey', 'WIT.AI API Key:', 'configure-app');
