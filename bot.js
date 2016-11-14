1fa3d4e1c267c4c54991a9fe75a407837521d969
Andrew Yip <yipcma@gmail.com>
2016-11-14 13:40:24 +0300

    remove sugar dependency

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const botBuilder = require('claudia-bot-builder');
-const rp = require('minimal-request-promise')
+const rp = require('minimal-request-promise');
 
 const app = botBuilder(function(request, originalApiRequest) {
     console.log(request);
@@ -13,12 +13,12 @@ const app = botBuilder(function(request, originalApiRequest) {
     const options = {
         headers: headers
     };
-
-    if (request.sender !== '1386501818046647')
+// TODO: fix timezone issue in display
+    // if (request.sender !== '1386501818046647')
         return rp.get('https://api.wit.ai/message?v=20161113&q=' + request.text, options).then(function(response) {
             console.log('got response', response.body);
             const reply = JSON.parse(response.body);
-            return reply.entities.from[0].value + ' => ' + reply.entities.to[0].value + ' @' + reply.entities.datetime[0].values[0].value + ', right?';
+            return reply.entities.from[0].value + ' => ' + reply.entities.to[0].value + ' @' + reply.entities.datetime[0].value + ', right?';
         }, function(response) {
             console.log('got error', response.body);
             return 'Sorry, I did\'t get that, again?';
