38585e4816498e1bf936a3dc5c015e296cd2adaf
Andrew Yip <yipcma@gmail.com>
2016-11-21 18:32:29 +0300

    from scratch rewrite promises: echo from

--- bot.js
+++ bot.js
@@ -19,32 +19,25 @@ function getWitParse(apikey, message) {
 // TODO: fix result filtering
 function getGeocode(apikey, loc) {
     const url = `http://locationiq.org/v1/search.php?key=${apikey}&format=json&q=${loc}&viewbox=38.99323,22.35786,39.17330,22.24668&bounded=1&limit=1`
-    console.log(url);
+
     // TODO: fix locationiq not called properly
-    return rp.get(url).then(response => {
-      console.log(response);
-      JSON.parse(response.body);
-    })
+    return rp.get(url)
   }
 
 const app = botBuilder(function(request, originalApiRequest) {
-    console.log(request);
     const witaiApiKey = originalApiRequest.env.witaiApiKey;
-    const locationiqApiKey = originalApiRequest.env.locationiqApiKey;
+    const locApiKey = originalApiRequest.env.locationiqApiKey;
     if (request.text)
-        return getWitParse(witaiApiKey, request.text).then(response => {
-          console.log(response);
-          const res = JSON.parse(response.body);
-          const from = res.entities.from[0].value;
-          const to = res.entities.to[0].value;
-          const datetime = res.entities.datetime[0].value;
-          Promise.all(getGeocode(locationiqApiKey, from), getGeocode(locationiqApiKey, to))
-        }).then(function(data){
-          console.log(data);
-          const fromData = data[0][0];
-          const toData = data[1][0];
-          return fromData.display_name + ' ' + toData.display_name;
-        })
+    return getWitParse(witaiApiKey, request.text).then((response) => {
+      const from = JSON.parse(response.body).entities.from[0].value;
+      console.log(from)
+      return from;
+    });
+      // return getWitParse(witaiApiKey, request.text).then((response) => {
+      //   const res = JSON.parse(response.body);
+      //   const from = res.entities.from[0].value;
+      //   return getGeocode(locApiKey, from);
+      // }).then(console.log);
 });
 
 app.addPostDeployConfig('witaiApiKey', 'WIT.AI API KEY:', 'configure-wit');
